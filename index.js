ca55fbb6fa141f11a382441e451ac759dd97d58c
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2019-01-17 05:20:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
+const prefix = '$';
 client.on('message', message => {//new msg event
 if(!message.channel.guild) return;
-  if(message.content.startsWith(prefix + 'Rainbow')) {//to create the rainbow role
-      let role = message.guild.roles.find('name', 'Rainbow Quick.')
+  if(message.content.startsWith(prefix + 'rainbow')) {//to create the rainbow role
+      let role = message.guild.roles.find('name', 'Rainbow')
     if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
   //start of create role 
   if(!role){
     rainbow =  message.guild.createRole({
-   name: "Rainbow Quick.",//the role will create name
+   name: "Rainbow",//the role will create name
    color: "#000000",//the default color
    permissions:[]//the permissions
  //end of create role
@@ -26,7 +26,7 @@ message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the s
 client.on('ready', () => {//new ready event
   setInterval(function(){
       client.guilds.forEach(g => {
-                  var role = g.roles.find('name', 'Rainbow Quick.');//rainbow role name
+                  var role = g.roles.find('name', 'Rainbow');//rainbow role name
                   if (role) {
                       role.edit({color : "RANDOM"});
                   };
